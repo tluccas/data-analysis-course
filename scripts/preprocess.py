de00bdd0a054ecd3bc563b133d28a3f1bea6e1de
Lucas Alves <lucas1901alves@gmail.com>
2026-01-28 14:39:08 -0300

    refactor(preprocess): centralize preprocess functions

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+# Func to translate column names from English to Portuguese
 def rename_columns(df):
     translate_columns = {
         'work_year': 'ano',
@@ -16,6 +17,7 @@ def rename_columns(df):
     }
     return df.rename(columns=translate_columns)
 
+# Func to translate/rename column values from English to Portuguese
 def translate_values(df):
     df["contrato"] = df["contrato"].replace({
         'FT': 'Tempo Integral',
@@ -39,4 +41,10 @@ def translate_values(df):
         50: 'Híbrido',
         100: 'Remoto'
     })
+    return df
+
+# Preprocess Function
+def preprocess(df):
+    df = rename_columns(df)
+    df = translate_values(df)
     return df
