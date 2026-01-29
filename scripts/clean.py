f7bf4f80d4be5ee0a08ff404425c117d2f594ac4
Lucas Alves <lucas1901alves@gmail.com>
2026-01-29 12:18:57 -0300

    feat(clear): add clean final script

--- scripts/clean.py
+++ scripts/clean.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from .preprocess import preprocess
 
 # Func to perform mean imputation
 def mean_imputation(series):
@@ -9,3 +10,16 @@ def mean_imputation(series):
 def median_imputation(series):
     return series.fillna(series.median())
     
+# Func to clears data frame removing rows with null values
+def clear_data_frame(df):
+    return df.dropna()
+
+def convert_column_to_int(df, column):
+    df[column] = df[column].astype(int)
+    return df
+
+def final_clear_df(df, column = 'ano'):
+    df = preprocess(df)
+    df = clear_data_frame(df)
+    df = convert_column_to_int(df, column)
+    return df
